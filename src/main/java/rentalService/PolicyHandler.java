2f6c68f0d614fb67838e0b2f97e4ac3601754fe4
yoonheesung <yoonheesung@10.250.95.69>
2020-10-22 11:41:11 +0900

    프로덕트 폴리시핸들러 수정

--- src/main/java/rentalService/PolicyHandler.java
+++ src/main/java/rentalService/PolicyHandler.java
@@ -1,12 +1,12 @@
 package rentalService;
 
-import rentalService.config.kafka.KafkaProcessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import rentalService.config.kafka.KafkaProcessor;
 
 @Service
 public class PolicyHandler{
@@ -14,6 +14,21 @@ public class PolicyHandler{
     public void onStringEventListener(@Payload String eventString){
 
     }
+    
+    @Autowired
+    ProductRepository ProductRepository;
 
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverProductSaved_Product(@Payload ProductSaved productSaved){
+
+        if(productSaved.isMe()){
+
+            Product product = new Product();
+            BeanUtils.copyProperties(productSaved, product);
+            //product.setId(productSaved.getId());
+
+            ProductRepository.save(product);
+        }
+    }
 
 }
